3bf7154920456133c1cdfad18f6de104ee00622a
Divyanshu Vijaywargiya <divyanshu.vijaywargiya@metacube.com>
2024-10-22 10:54:43 +0530

    Suggestions implemented

--- ShoppingCart.java
+++ ShoppingCart.java
@@ -1,57 +1,121 @@
 package shoppingcart;
 
 import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.Map;
 
 public class ShoppingCart {
-	//this map will store item and their quantity
-	private Map<Item, Integer> cartItems;
+	//this map will store item and their quantity in cart
+	public Map<Item, Integer> cartItems;
 	
 	public ShoppingCart() {
 		cartItems = new HashMap<>();
 	}
 	
 	//Adding item in cart	
-	public void addToCart(Item item , Integer quantity) {
-		if(quantity <= 0) {
-			System.out.println("Quantity must be greater than 0");
-			return;
-		}
-		
-		if(cartItems.containsKey(item)) {
-		//if item already in cart then just increase its quantity
-			cartItems.put(item, cartItems.get(item)+quantity);
-		}else {
-			cartItems.put(item, quantity);
+	public void addItemToCart(Item item , Integer quantity) {
+		try {
+			if(item == null || quantity == null || quantity <= 0) {
+				throw new IllegalArgumentException("Item or quantity cannot be null or invalid.");
+			}
+			if(cartItems.containsKey(item)) {
+				//if item already in cart then just increase its quantity
+				cartItems.put(item, cartItems.get(item)+quantity);
+			}else {
+				cartItems.put(item, quantity);
+			}
+		}catch(IllegalArgumentException e){
+			System.out.println("Error adding item to cart : " + e.getMessage());
 		}
 	}
 	
 	
 	//Show the quantity of particular item
-	public Integer displayQty(Item item) {
+	public Integer displayItemQuantity(Item item) {
 		return cartItems.getOrDefault(item, 0);
 	}
 	
 	//to update quantity of an item in cart
-	public void updateQty(Item item, Integer quantity) {
-		if(cartItems.containsKey(item)){
-			cartItems.put(item, quantity);
-		}else {
-			System.out.println("Item not present in cart.");
+	public void updateItemQuantity(Item item, Integer quantity) {
+		try {
+			if(item == null || quantity == null || quantity <= 0) {
+				throw new IllegalArgumentException("Item or quantity cannot be null or invalid.");
+			}
+			if(cartItems.containsKey(item)){
+				cartItems.put(item, quantity);
+			}else {
+				System.out.println("Item not present in cart.");
+			}
+		}catch(IllegalArgumentException e) {
+			System.out.println("Error Updating item to cart : " + e.getMessage());
 		}
 	}
 	
 	//Delete item from cart
-	public void deleteItem(Item item) {
+	public void deleteItemFromCart(Item item) {
+		if(item == null) {
+			throw new IllegalArgumentException("Item cannot be null.");
+		}else {			
 		cartItems.remove(item);
+		}
 	}
 	
 	//total bill of whole cart
-	public double displayBill() {
+	public double displayTotalBill() {
 		double total = 0;
 		for(Map.Entry<Item, Integer> entry : cartItems.entrySet()) {
 			total += entry.getKey().getPrice() * entry.getValue();
  		}
 		return total;
 	}
+	
+	//to find item in cart
+	public Item findItemById(ArrayList<Item> Items, String ItemId) {
+		try {
+			if(ItemId == null) {
+				throw new IllegalArgumentException("ItemId cannot be null.");
+			}
+			for(Item item : Items) {
+				if(item.getItemId().equals(ItemId)) {
+					return item;
+				}
+			}
+			return null;
+		}catch(IllegalArgumentException e) {
+			System.out.println("Error while finding item : " + e.getMessage());
+			return null;
+		}
+	}
+	
+	//to find item in cart
+	public Item findItemByIdInCart(String ItemId) {
+		try {
+			if(ItemId == null) {
+				throw new IllegalArgumentException("ItemId cannot be null.");
+			}
+			for(Item item : cartItems.keySet()) {
+				if(item.getItemId().equals(ItemId)) {
+					return item;
+				}
+			}
+			return null;
+		}catch(IllegalArgumentException e) {
+			System.out.println("Error while finding item : " + e.getMessage());
+			return null;
+		}
+	}
+	
+	public void displayCart() {
+		if(cartItems.isEmpty()) {
+			System.out.println("Your Cart is Empty.");
+		}else {
+			System.out.println("Your Cart : ");
+			for(Map.Entry<Item, Integer> entry : cartItems.entrySet()) {
+				Item item = entry.getKey();
+				System.out.println("Item Name : " + item.getName() + " -> Quantity : " + entry.getValue() + " -> Price " + entry.getValue() * item.getPrice());
+			}
+			System.out.println("Cart's Total Bill : " + displayTotalBill());
+		}
+	}
+
 }
--- ShoppingCartTesting.java
+++ ShoppingCartTesting.java
@@ -1,36 +1,110 @@
 package shoppingcart;
 
-public class ShoppingCartTesting {
+import java.util.ArrayList;
+import java.util.Scanner;
 
+
+public class ShoppingCartTesting {
+	
 	public static void main(String[] args) {
+		ArrayList<Item> availableItems = new ArrayList();
 		//we manually creating items
-		Item item1 = new Item("101", "Laptop", "New Best Gaming Laptop", 1592.00);		
-		Item item2 = new Item("102", "Mouse", "New Best Gaming Mouse", 159.00);
-		Item item3 = new Item("103", "Keyboard", "New Best Gaming Keyboard", 192.00);
+		availableItems.add(new Item("101", "Laptop", "New Best Gaming Laptop", 1592.00));		
+		availableItems.add(new Item("102", "Mouse", "New Best Gaming Mouse", 159.00));
+		availableItems.add(new Item("103", "Keyboard", "New Best Gaming Keyboard", 192.00));
 		
 		//now we are creating a shopping cart
 		ShoppingCart cart = new ShoppingCart();
 		
+		Scanner scanner = new Scanner(System.in);
+		boolean running = true;
 		
-		//adding to cart
-		cart.addToCart(item1, 1);
-		cart.addToCart(item2, 2);
-		
-		//we can display quantity of specific item in cart
-		System.out.println("Quantity of " + item1.getName() + " : "+ cart.displayQty(item1));
-		System.out.println("Quantity of " + item2.getName() + " : "+ cart.displayQty(item2));
-		System.out.println("Quantity of " + item3.getName() + " : "+ cart.displayQty(item3));
-		
-		//Update quantity
-		cart.updateQty(item1, 7);
-		System.out.println("New Updated Quantity of " + item1.getName() + " : "+ cart.displayQty(item1));
-		
-		//Displaying total bill
-		System.out.println("Total bill of cart : " + cart.displayBill());
-		
-		//Deleting an item from cart
-		cart.deleteItem(item2);
-		System.out.println("Total bill of cart after removal of " + item2.getName() + " : "+ cart.displayBill());
+		while(running) {
+			//firstly we will show available items in inventory
+			System.out.println("Available Items in inventory: ");
+			for(Item item: availableItems) {
+				System.out.println(item.getItemId()+" : "+item.getName());
+			}
+			
+			
+			//Display menu for several operations
+			System.out.println("Choose an Option");
+			System.out.println("1: Add Item to cart");
+			System.out.println("2: Update Quantity in cart");
+			System.out.println("3: Delete item from cart");
+			System.out.println("4: View Cart");
+			System.out.println("5: View total bill");
+			System.out.println("6: Exit");
+			
+			
+			int choice = scanner.nextInt();
+			scanner.nextLine();
+			
+			
+			switch(choice) {
+				//Adding an item
+				case 1:
+					System.out.println("Enter Item id : ");
+					String addItemId = scanner.nextLine();
+					Item addItem = cart.findItemById(availableItems, addItemId);
+					if(addItem != null) {
+						System.out.println("Enter Quantity : ");
+						int quantity = scanner.nextInt();
+						cart.addItemToCart(addItem, quantity);
+						System.out.println(addItem.getName() + " added to cart.");
+					}else {
+						System.out.println("Item not found.");
+					}
+					break;
+					
+				//Update item quantity
+				case 2:
+					System.out.println("Enter item id to update : ");
+					String updateItemId = scanner.nextLine();
+					Item updateItem = cart.findItemByIdInCart(updateItemId);
+					if(updateItem != null) {
+						System.out.println("Enter Quantity : ");
+						int updatedQuantity = scanner.nextInt();
+						cart.updateItemQuantity(updateItem, updatedQuantity);
+						System.out.println(updateItem.getName() + " quantity updated.");
+					}else {
+						System.out.println("Item not found in Cart.");
+					}
+					break;
+					
+				//Delete an item from cart
+				case 3: 
+					System.out.println("Enter ItemId to delete : ");
+					String deleteItemId = scanner.nextLine();
+					Item deleteItem = cart.findItemById(availableItems, deleteItemId);
+					if(deleteItem != null) {
+						cart.deleteItemFromCart(deleteItem);
+						System.out.println(deleteItem.getName() + " deleted from cart.");
+					}else {
+						System.out.println("Item not found.");
+					}
+					break;
+				
+				//View Cart
+				case 4:
+					cart.displayCart();
+					break;
+					
+				//Total bill of cart
+				case 5:
+					System.out.println("Total bill : " + cart.displayTotalBill());
+					break;
+				
+				//exiting
+				case 6:
+					running = false;
+					System.out.println("Exiting....");
+					break;
+					
+				default:
+					System.out.println("Invalid option.");
+			}	
+		}		
+		scanner.close();
 	}
-
 }
